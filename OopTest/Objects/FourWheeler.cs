8558c4412708483d88a0b402b6291efbb92a3342
farukjnu1@gmail.com <farukjnu1@gmail.com>
2025-09-17 23:24:47 +0800

    Add project files.

---
+++ OopTest/Objects/FourWheeler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopTest.Objects
+{
+    class FourWheeler:Vehicle, IInterior
+    {
+        public int NumberOfDoor { get; set; }
+        public void DesignInterior()
+        {
+
+        }
+    }
+
+    interface IInterior
+    {
+        void DesignInterior();
+    }
+
+}
